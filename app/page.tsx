0961bfcc96c9ad016064b69b1c6c3bb3ec74d83c
Raunak Das <dasraunak5@gmail.com>
2024-06-25 19:56:41 +0530

    Started working on Grid Section

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,20 @@ import Hero from "@/components/Hero";
 //import Approach from "@/components/Approach";
 //import Experience from "@/components/Experience";
 //import RecentProjects from "@/components/RecentProjects";
-//import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
-        
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
